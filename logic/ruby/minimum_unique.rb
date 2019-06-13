453d29714c6e4d746aef6ecbf6c9f57f46a9a85f
Rido Sandi Atmanto <ridoatmanto@gmail.com>
2019-06-13 18:37:11 +0700

    update minimum unique method

--- logic/ruby/minimum_unique.rb
+++ logic/ruby/minimum_unique.rb
@@ -3,6 +3,15 @@ def minimum_unique(input)
   # PLEASE WRITE YOUR CODE HERE
   # YOU ARE FREE TO CREATE ANY OTHER SUPPORTING FUCTION AS NECESSARY
   ###
+
+  temp_sum = 0
+
+  input.each_with_index do |item, index|
+  	input[index] += 1 while input.count(input[index]) > 1
+  	temp_sum += input[index]
+  end
+
+  temp_sum
 end
 
 output_filename = ENV['OUTPUT_PATH'] || 'output.txt'
@@ -22,3 +31,4 @@ result = minimum_unique input
 output_file.write result
 output_file.write "\n"
 output_file.close
+	
